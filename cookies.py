d5164a156759bd5d83848e8cd72e95d6316d9f52
Lyjor Barros <lyjorbarros@MacBook-M4-de-Lyjor.local>
2026-05-27 15:05:32 -0300

    vai 3

---
+++ cookies.py
@@ -0,0 +1,9 @@
+from flask import Flask, make_response
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    resp = make_response("Hello")
+    resp.set_cookie("sessionid", "12345")  # inseguro: sem HttpOnly/Secure
+    return resp
+
